1044f39c11f7ae42cebd39fd71c31a741522029c
Kavkazec <oganesmkrtchyan2004@gmail.com>
2024-10-01 02:26:52 +0300

    added catalog, form, faq and gift cards

--- js/script.js
+++ js/script.js
@@ -48,69 +48,126 @@ $(document).ready(function() {
 
       const productSlider = new Swiper('.product-silder', {
         speed: 600,
-        // pagination: {
-        //     el: ".product-silder-pagination",
-        //   },
           scrollbar: {
             el: ".product-silder-scrollbar",
           },
       });
 
       function sliderMouseSlideInit() {
-          document.addEventListener("mousemove", function(e) {
+        document.addEventListener("mousemove", function(e) {
             const targetElement = e.target;
             if (targetElement.closest('[data-mousemove-swipe]')) {
-              const sliderElement = targetElement.closest('[data-mousemove-swipe]');
-              const sliderItem = sliderElement.swiper.slides[getIndex(sliderElement)];
-              const sliderLength = sliderElement.swiper.slides.length;
-              if (sliderLength > 1) {
-                const sliderWidth = sliderItem.offsetWidth;
-                const sliderPath = Math.round(sliderWidth / sliderLength);
-                const sliderMousePos = e.clientX - sliderElement.offsetLeft;
-                const sliderSlide = Math.floor(sliderMousePos / sliderPath);
-                sliderElement.swiper.slideTo(sliderSlide);
-              }
+                const sliderElement = targetElement.closest('[data-mousemove-swipe]');
+                const sliderItem = sliderElement.swiper.slides[getIndex(sliderElement)];
+                const sliderLength = sliderElement.swiper.slides.length;
+                if (sliderLength > 1) {
+                    const sliderWidth = sliderItem.offsetWidth;
+                    const sliderPath = Math.round(sliderWidth / sliderLength);
+                    const sliderMousePos = e.clientX - sliderElement.offsetLeft;
+                    const sliderSlide = Math.floor(sliderMousePos / sliderPath);
+                    sliderElement.swiper.slideTo(sliderSlide);
+                }
             }
-          })
-
-          function getIndex(el) {
-            return Array.from(el.parentNode.children).indexOf(el);
-          } 
-      }
-
-      if (document.querySelector('[data-mousemove-swipe]')) {
-        sliderMouseSlideInit();
-      }
-
-
-      
-      // function sliderMouseSlideInit() {
-      //   $(document).on('mousemove', function(e) {
-      //     var $targetElement = $(e.target);
-      //     if ($targetElement.closest('[data-mousemove-swipe]').length) {
-      //       var sliderElement = $targetElement.closest('[data-mousemove-swipe]');
-      //       var sliderIndex = getIndex(sliderElement);
-      //       var sliderItem = productSlider.slides[sliderIndex];
-      //       var sliderLength = productSlider.slides.length;
-      //       console.log(sliderLength);
-            
-      //       if (sliderLength > 1) {
-      //         var sliderWidth = sliderItem.width;
-      //         var sliderPath = Math.round(sliderWidth / sliderLength);
-      //         var sliderMousePos = e.clientX - sliderElement.offset().left;
-      //         var sliderSlide = Math.floor(sliderMousePos / sliderPath);
-              
-      //         productSlider.slideTo(sliderSlide);
-      //       }
-      //     }
-      //   });
-      // }
-      
-      // function getIndex(el) {
-      //   return $(el).parent().children().index($(el));
-      // }
+        });
     
-      // if ($('[data-mousemove-swipe]').length) {
-      //   sliderMouseSlideInit();
-      // }
+        // Добавляем событие для отслеживания ухода мыши с элемента слайдера
+        document.querySelectorAll('[data-mousemove-swipe]').forEach(function(sliderElement) {
+            sliderElement.addEventListener("mouseleave", function() {
+                sliderElement.swiper.slideTo(0); // Возвращаем на первый слайд
+            });
+        });
+    
+        function getIndex(el) {
+            return Array.from(el.parentNode.children).indexOf(el);
+        }
+    }
+    
+    if (document.querySelector('[data-mousemove-swipe]')) {
+        sliderMouseSlideInit();
+    }
+
+
+
+
+
+
+    const partnersSlider = new Swiper(".partners-silder", {
+      spaceBetween: 20,
+      initialSlide: 0,
+      speed: 600,
+      slidesPerView: 'auto',
+      grabCursor: true,
+      keyboard: {
+        enabled: true,
+      },
+      // autoplay: {
+      //   delay: 4000,
+      //   disableOnInteraction: false,
+      // },
+      navigation: {
+        nextEl: ".partners-slider-next",
+        prevEl: ".partners-slider-prev",
+    },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+      breakpoints: {
+        1200: {
+          spaceBetween: 13
+        }, 
+        1500: {
+          spaceBetween: 16
+        }, 
+      }
+    });
+
+
+
+
+    $('.phone-input').inputmask({
+      "mask": "+7 (999) 999 - 99 - 99",
+      "placeholder": "",
+      "showMaskOnHover": false,
+      "showMaskOnFocus": true
+  });
+
+
+
+  // Сначала скроем все ответы
+$('.faq__item-answer').slideUp();
+
+$('.faq__item').click(function() {
+    // Уберем класс rotate-faq у всех SVG в заголовках вопросов
+    $('.faq__item-question svg').removeClass('rotate-faq');
+    
+    // Применим исходные стили к всем элементам
+    $(this).find('.faq__item-question').css('border', '1px solid #E8E8E8');
+    $(this).find('.faq__item-question span, .faq__item-question p').css('color', '#000');
+    $('.faq__item').css('border', '1px solid transparent');
+
+    // Находим текущий вопрос и ответ
+    var $currentAnswer = $(this).find('.faq__item-answer');
+    var $currentQuestion = $(this).find('.faq__item-question');
+  
+    // Если текущий ответ виден, скроем его
+    if ($currentAnswer.is(':visible')) {
+        $currentAnswer.slideUp();
+    } else {
+        // Скроем все ответы, кроме текущего
+        $('.faq__item-answer').not($currentAnswer).slideUp();
+        
+        // Раскроем только текущий ответ
+        $currentAnswer.slideDown();
+        
+        // Изменяем стиль для текущего вопроса
+        $(this).find('.faq__item-question').css('border', '1px solid transparent');
+        $(this).find('.faq__item-question span, .faq__item-question p').css('color', '#DA0916');
+        $(this).css('border', '1px solid #E8E8E8');
+        
+        // Добавляем класс rotate-faq к SVG текущего вопроса
+        $currentQuestion.find('svg').addClass('rotate-faq');
+    }
+});
+  
+    
 });
